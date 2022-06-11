8a625ed8f38559c3023877634fbc6ad4636331c8
Quentin ANGELOT <57401552+qangelot@users.noreply.github.com>
2022-06-11 16:22:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,40 +1,41 @@
 from flask import request, jsonify, Flask
 import os, requests
 
-# docker ne copie pas le .env, on le passe dans la commande: export APIKEY=MYAPIKEY
+
+# docker ne copie pas le .env, on le passe dans la commande:
+# export APIKEY=MYAPIKEY
 # docker run -p 5000:5000 --env APIKEY=$APIKEY --rm myapi
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 app.config.from_object('config.Config')
+# reads the environment variables
+LAT = os.environ['LAT']  
+LONG = os.environ['LONG']
 
 
 @app.route('/', methods=['GET'])
 def home():
     html = """
-           <h1>Sunrise-sunset API</h1>
-           <p>Example RESTful API built using Flask and Docker</p>
+           <h1>Weather API</h1>
+           <p>RESTful API made to provide infos about the weather to the end user.
+           <br> Usage:
+           <ul>
+            <li> /api/daylight/'city_name': get daylight infos about the provided LAT/LONG couple</li>
+            <li> /api/weather/: get weather infos about the provided LAT/LONG couple</li>
+           </ul>
+           </p>
            """
     return html
 
 
-@app.route('/api/<city>', methods=['GET'])
-def api(city):
+@app.route('/api/daylight/', methods=['GET'])
+def api_daylight():
     output = {}
-    if city:
-        output = {
-            'sunrise': 1,
-            'sunset': 2,
-            'city': city
-        }
-    return jsonify(output)
-
-@app.route('/api/daylight/<city>', methods=['GET'])
-def api_daylight(city):
-    output = {}
-    uri = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={app.config['APIKEY']}"
+    uri = f"http://api.openweathermap.org/data/2.5/weather?lat={LAT}&lon={LONG}&appid={app.config['APIKEY']}&units=metric"
     print(uri)
     res = requests.get(uri)
+    print(res)    
     if res.status_code == 200:
         data = res.json()
         sunrise = data['sys']['sunrise']
@@ -42,10 +43,32 @@ def api_daylight(city):
         output = {
             'sunrise': sunrise,
             'sunset': sunset,
-            'daylight': sunset-sunrise,
-            'city': city            
+            'daylight': sunset-sunrise            
         }
-    return output
+    return jsonify(output)
+
+
+# test this route and redeploy then test with docker pull and params
+@app.route('/api/weather/', methods=['GET'])
+def api_weather():
+    output = {}
+    uri = f"http://api.openweathermap.org/data/2.5/weather?lat={LAT}&lon={LONG}&appid={app.config['APIKEY']}&units=metric"
+    print(uri)
+    res = requests.get(uri)
+    print(res)
+    if res.status_code == 200:
+        data = res.json()
+        temp = data['main']['temp']
+        temp_feel = data['main']['feels_like']
+        pressure = data['main']['pressure']
+        humidity = data['main']['humidity']
+        output = {
+            'actual temperature': temp,
+            'feeling': temp_feel,
+            'pressure': pressure,
+            'humidity': humidity            
+        }
+    return jsonify(output)
 
 
 if __name__ == '__main__':
